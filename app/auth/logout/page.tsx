159ce21caa96d55dbfbc51a87a011edcaa76f4c5
Pongsapak Lakchai <aempongsapak@gmail.com>
2025-06-20 00:24:58 +0700

    delete not important

--- app/auth/logout/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LogoutButton } from "@/components/logout-button"
-
-export default function LogoutPage() {
-  return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <LogoutButton />
-    </div>
-  )
-}
